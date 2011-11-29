428fa68c07b6569e5a14644edd34ab6ccecc6654
Peter Aronoff <telemachus@arpinum.org>
2011-11-29 14:30:20 -0500

    Two ways to mass import files

---
+++ dot_copier.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+[ "$1" = "-v" ] && V="-v"
+: ${DOTFILES:=$HOME/code/dotfiles}
+
+for item in ".bash_aliases" ".bash_completion" ".bash_functions" \
+    ".bashrc" ".profile" ".gemrc" ".irbrc" ".inputrc" ".hushlogin" \
+    ".gitignore_global" ".minicpanrc" ".procmailrc"
+do
+    item_no_dot=$(printf $item | awk '{ print substr($1, 2); }')
+    if [ -h "$HOME/$item" -a -e "$DOTFILES/${item_no_dot}" ]; then
+        rm $V "$HOME/$item"
+    fi
+done
+
+for item in "bash_aliases" "bash_completion" "bash_functions" \
+    "bashrc" "profile" "gemrc" "irbrc" "inputrc" "hushlogin" \
+    "gitignore_global" "minicpanrc" "procmailrc"
+do
+    if [ -e "$DOTFILES/$item" ]; then
+        cp $V "$DOTFILES/$item" "$HOME/.$item"
+    fi
+done
---
+++ dot_linker.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+[ "$1" = "-v" ] && V="-v"
+: ${DOTFILES:=$HOME/code/dotfiles}
+
+for item in ".bash_aliases" ".bash_completion" ".bash_functions" \
+    ".bashrc" ".profile" ".gemrc" ".irbrc" ".inputrc" ".hushlogin" \
+    ".gitignore_global" ".minicpanrc" ".procmailrc"
+do
+    item_no_dot=$(printf $item | awk '{ print substr($1, 2); }')
+    if [ -e "$HOME/$item" -a -e "$DOTFILES/${item_no_dot}" ]; then
+        mv $V "$HOME/$item" "$HOME/old_${item_no_dot}"
+    fi
+done
+
+for item in "bash_aliases" "bash_completion" "bash_functions" \
+    "bashrc" "profile" "gemrc" "irbrc" "inputrc" "hushlogin" \
+    "gitignore_global" "minicpanrc" "procmailrc"
+do
+    if [ -e "$DOTFILES/$item" ]; then
+        ln -s $V "$DOTFILES/$item" "$HOME/.$item"
+    fi
+done
